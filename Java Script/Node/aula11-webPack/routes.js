4df22377830534da960a349dab7f7b00a4a7cd76
GustavoKurek <gustavokureksz@gmail.com>
2021-10-12 21:50:29 -0300

    commit

--- Java Script/Node/aula11-webPack/routes.js
+++ Java Script/Node/aula11-webPack/routes.js
@@ -3,7 +3,7 @@ const route= express.Router();
 
 const homeController=require('./src/controllers/homeController')
 route.get('/', homeController.paginaInicial)
-route.get('/1', homeController.paginaInicial)
+route.get('/12', homeController.paginaInicial)
 
 
 module.exports=route;
