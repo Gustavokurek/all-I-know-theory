fed1c96341f88a80a82e94546309f2e8479d11d8
Gustavo Kurek <GustavoKureksz@gmail.com>
2022-03-08 19:28:16 -0300

    type undefined and null

---
+++ TypeScript/baseTypeScript/src/A007-type-null-undefined/A007-type-null-undefined.ts
@@ -0,0 +1,33 @@
+//undefined usar mais para checagem ex:
+
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 20);
+
+export function CreatePerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+// usar quando vc quiser retornar um não valor, ou para quando vc não conseguir o valor desejado
+
+function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwo = squareOf(2);
+
+if (squareOfTwo === null) {
+  console.log('conta inválida');
+} else {
+  console.log(squareOfTwo, squareOfTwo - 56);
+}
