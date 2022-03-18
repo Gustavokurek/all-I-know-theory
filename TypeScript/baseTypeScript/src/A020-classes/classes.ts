2a1cd2c1975efe1b9fa80d39348cfb0a6a0f4c01
Gustavo Kurek <GustavoKureksz@gmail.com>
2022-03-17 21:20:57 -0300

    começo de classes

---
+++ TypeScript/baseTypeScript/src/A020-classes/classes.ts
@@ -0,0 +1,42 @@
+export class Empresas {
+  // duas formas de instanciar
+  // longa
+  // posso instanciar direto sem o constructor
+  private readonly colaboradores: Colaboradores[] = [];
+
+  // com o constructor
+  public readonly nome: string;
+  protected readonly CNPJ: string;
+
+  constructor(nome: string, CNPJ: string) {
+    this.nome = nome;
+    this.CNPJ = CNPJ;
+  }
+
+  addColaborador(colaborador: Colaboradores): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+class Colaboradores {
+  // modo rápido, faz tudo direto
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresas('Gustavo', '4654687464321');
+const colaborador = new Colaboradores('Guilherme', 'del Toro');
+const colaborador2 = new Colaboradores('Guilherme', 'del Toro');
+empresa1.addColaborador(colaborador);
+empresa1.addColaborador(colaborador2);
+// structural all type
+empresa1.addColaborador({ nome: 'lupo', sobrenome: 'Kursk' });
+console.log(empresa1.mostrarColaboradores());
